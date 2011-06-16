6216f761ad79380f795bae2a21b2e1df37fb7650
Zac Bowling <zac@zacbowling.com>
2011-06-16 10:59:17 -0700

    Initial Commit

---
+++ ContainerTest/ContainerTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ContainerTestAppDelegate.h
+//  ContainerTest
+//
+//  Created by Zac Bowling on 6/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContainerTestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContainerTest/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  ContainerTest
+//
+//  Created by Zac Bowling on 6/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ContainerTestAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([ContainerTestAppDelegate class]));
+    }
+    return retVal;
+}
